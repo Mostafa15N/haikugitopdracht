da6849eaa2bd110266e07b8fe22da79e2b546195
Mostafa15N <114576465+Mostafa15N@users.noreply.github.com>
2022-10-12 11:54:53 +0200

    Update haiku.py

--- haiku.py
+++ haiku.py
@@ -1,10 +1,9 @@
 
 
 #plak hier de haiku functions onder elkaar
-
-
-
-
+def HaikuX(): print("haiku by:","AUTHOR") print("REGEL1") print("REGEL2") print("REGEL3")
+def HaikuX(): print("haiku by:","AUTHOR") print("REGEL1") print("REGEL2") print("REGEL3")
+def HaikuX(): print("haiku by:","AUTHOR") print("REGEL1") print("REGEL2") print("REGEL3")
 #laat de code hieronder met rust
 Haiku1()
 Haiku2()
